5fc02d7573f34e5acf2167666eb3723d6cc61c69
GabiRP <57387907+GabiRP@users.noreply.github.com>
2023-12-10 18:32:15 +0100

    initt

--- QueueManager/src/client/java/es/gabirp/queuemanager/Globals.java
+++
@@ -1,9 +0,0 @@
-package es.gabirp.queuemanager;
-
-import net.minecraft.client.MinecraftClient;
-
-public class Globals {
-    public static MinecraftClient mc = MinecraftClient.getInstance();
-    public static Boolean isQueueManagerEnabled = false;
-    public static String endpointUrl = "ws://127.0.0.1:8080/queue";
-}
